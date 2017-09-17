d966df99e460ecf7458ee22afdbd46d94bfe6675
Mikhail Merkulov <merkulov@corepartners.com>
2017-09-17 09:36:24 -0400

    Added NativeBase + Groups screen stub.

--- smashtt-mobile/App.js
+++ smashtt-mobile/App.js
@@ -1,21 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import {Container, StyleProvider, Root, getTheme} from "native-base";
+import GroupsScreen from "./screens/Groups/GroupsScreen";
+import material from "native-base/src/theme/variables/material";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!!</Text>
-      </View>
-    );
-  }
+export class App extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			isFontsReady: false,
+		};
+	}
+
+	async componentWillMount() {
+		await Expo.Font.loadAsync({
+			"Roboto": require("./assets/fonts/Roboto/Roboto.ttf"),
+			"Roboto_medium": require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
+		});
+		this.setState({
+			isFontsReady: true,
+		})
+	}
+
+	render() {
+		return (
+			<Container>
+				<StatusBar hidden={true}/>
+				{this.state.isFontsReady ? <GroupsScreen/> : null}
+			</Container>
+		);
+	}
 }
 
+export default class RootComponent extends React.Component {
+	render() {
+		return (
+			<StyleProvider style={getTheme(material)}>
+				<Root>
+					<App />
+				</Root>
+			</StyleProvider>
+		);
+	}
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
---
+++ smashtt-mobile/screens/Groups/GroupsScreen.js
@@ -0,0 +1,12 @@
+import {Container, Text} from "native-base";
+import * as React from "react";
+
+export default class GroupsScreen extends React.Component {
+	render() {
+		return (
+			<Container>
+				<Text>Groups</Text>
+			</Container>
+		);
+	}
+}
